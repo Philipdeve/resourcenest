6b02e5c95b4207f346a00bffa9c6900b3bb1d6be
Philipdeve <thedeveloperphilip@gmail.com>
2025-10-20 14:14:07 +0100

    refactor: fixed callback url 2

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -39,9 +39,7 @@ const Auth = () => {
   const handleSignUp = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
-    const redirectUrl = window.location.hostname === 'localhost'
-  ? 'http://localhost:8080/resources'
-  : 'https://resourcenestwebsite.netlify.app/resources';
+    const redirectUrl = import.meta.env.VITE_SUPABASE_REDIRECT_URL;
     try {
       const { data, error } = await supabase.auth.signUp({
         email,
